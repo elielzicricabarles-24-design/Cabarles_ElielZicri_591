28e8d38b41bc58cac9f2b9c17a66c5b4d0b8e908
Tugbang <elielzicri.cabarles-24@cpu.edu.ph>
2026-04-20 10:22:32 +0800

    finals_examadded_new

--- modelhandling_backup/android/app/src/main/kotlin/com/example/modelhandling_auth/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.modelhandling_auth
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- modelhandling_prev/android/app/src/main/kotlin/com/example/modelhandling/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.modelhandling
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
